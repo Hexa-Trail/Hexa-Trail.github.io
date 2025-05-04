fc1a7b341e3c0f8e17d3cfa5fd4fa945b1eaf14f
Nathan <mega.nathinus@gmail.com>
2025-05-04 11:33:21 +0200

    visual changes

--- index_resources/index_script.js
+++ index_resources/index_script.js
@@ -8,33 +8,33 @@ document.addEventListener('DOMContentLoaded', () => {
   
     // Crée les points indicateurs
     images.forEach((_, i) => {
-      const dot = document.createElement('span');
-      dot.classList.add('dot');
-      dot.addEventListener('click', () => {
+        const dot = document.createElement('span');
+        dot.classList.add('dot');
+        dot.addEventListener('click', () => {
         index = i;
         updateCarousel();
         restartInterval();
-      });
-      indicatorsContainer.appendChild(dot);
+        });
+        indicatorsContainer.appendChild(dot);
     });
   
     const dots = document.querySelectorAll('.dot');
   
     function updateCarousel() {
-      const slideWidth = slidesContainer.clientWidth;
-      slidesContainer.style.transform = `translateX(-${index * slideWidth}px)`;
-      dots.forEach(dot => dot.classList.remove('active'));
-      dots[index].classList.add('active');
+        const slideWidth = slidesContainer.clientWidth;
+        slidesContainer.style.transform = `translateX(-${index * slideWidth}px)`;
+        dots.forEach(dot => dot.classList.remove('active'));
+        dots[index].classList.add('active');
     }
   
     function nextSlide() {
-      index = (index + 1) % images.length;
-      updateCarousel();
+        index = (index + 1) % images.length;
+        updateCarousel();
     }
   
     function restartInterval() {
-      clearInterval(interval);
-      interval = setInterval(nextSlide, 7000);
+        clearInterval(interval);
+        interval = setInterval(nextSlide, 7000);
     }
   
     // Initialise
@@ -43,5 +43,5 @@ document.addEventListener('DOMContentLoaded', () => {
   
     // Resize pour ajuster le slide si l'écran change
     window.addEventListener('resize', updateCarousel);
-  });
+});
   
